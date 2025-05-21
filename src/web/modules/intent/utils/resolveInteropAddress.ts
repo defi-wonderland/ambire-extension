01d7191b479704558a51963862128664bd651fba
Onizuka-wl <onizuka@defi.sucks>
2025-05-21 11:44:52 -0300

    feat: 🎸 mocked SDK addy validation

--- src/web/modules/intent/utils/resolveInteropAddress.ts
+++ src/web/modules/intent/utils/resolveInteropAddress.ts
@@ -1,8 +1,15 @@
 export const resolveInteropAddress = async (trimmedAddress: string): Promise<string> => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(trimmedAddress)
-    }, 100)
-  })
+  const pattern = /^[^@]+@[^:#]+(?::[^#]+)?#[^#]+$/
+
+  if (pattern.test(trimmedAddress)) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(trimmedAddress)
+      }, 100)
+    })
+  }
+
+  return Promise.reject('')
+
   // return isValid ? trimmedAddress : ''
 }
