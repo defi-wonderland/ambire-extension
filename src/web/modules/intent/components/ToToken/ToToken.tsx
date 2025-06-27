cf59f233c0cacfbc4bc0fa346a73895a8a59b6fc
Onizuka-wl <onizuka@wonderland.xyz>
2025-06-27 16:44:45 -0300

    feat: 🎸 add styles and toToken modification

--- src/web/modules/intent/components/ToToken/ToToken.tsx
+++ src/web/modules/intent/components/ToToken/ToToken.tsx
@@ -69,8 +69,6 @@ const ToToken: FC<Props> = ({ isLoading }) => {
   //     }),
   //   [dispatch]
   // )
-  //
-  //
 
   const handleSetToNetworkValue = useCallback(
     (networkOption: SelectValue) => {
