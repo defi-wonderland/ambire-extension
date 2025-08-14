5f94cd4183fc7c99e6c003131c28ae29beacfd32
Onizuka-wl <onizuka@wonderland.xyz>
2025-08-14 17:32:59 -0300

    fix: 🐛 optional chaining operator

--- src/web/modules/privacy/components/SeedPhraseManager.tsx
+++ src/web/modules/privacy/components/SeedPhraseManager.tsx
@@ -62,7 +62,7 @@ const SeedPhraseManager = ({ ppData }: SeedPhraseManagerProps) => {
         <Button
           type="secondary"
           onPress={loadAccount}
-          disabled={!seedPhrase.trim() || isGenerating || isLoading}
+          disabled={!seedPhrase?.trim() || isGenerating || isLoading}
           text={isLoading ? 'Loading Account...' : 'Load Existing Account'}
         />
       </View>
